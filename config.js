16973857f56dc45380bb7e599febe7e05723474a
Shadoxtkt <113625326+Shadoxtkt@users.noreply.github.com>
2024-10-21 16:58:28 +0200

    grdg

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DISCORD_WEBHOOK_URL: 'https://discord.com/api/webhooks/1297935345266069555/jkJRcyXBs0Lk3PeZCpFe9I56Ob5wg3IOhsCr1O8pl1cJMGGwj5EpGrDEn2C1BbWKBohc' // Remplace par ton URL de webhook
+};
